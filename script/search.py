57fa043710c5befea9d1001de8fe8a5c17a0beba
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-01-27 20:09:01 -0300

    Add pseudo fuzzy search function.

--- script/search.py
+++ script/search.py
@@ -1,111 +1,46 @@
-#!/usr/bin/env python3
-"""Search icons by word/name."""
-
-import curses
-import locale
-from icons import icons
-import ui
-
-MAX_NAME_LEN = 0
-for k in icons.keys():
-    if len(k) > MAX_NAME_LEN:
-        MAX_NAME_LEN = len(k)
+"""Search algorithms."""
 
 
-def main(_):
-    """Do stuff!"""
+def simple_search(icons, pattern, max_el):
+    """Icon name contains pattern."""
 
-    locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
+    findings = []
+    for k in icons.keys():
+        if k.find(pattern) > 0:
+            findings.append(k)
+    return findings[:max_el]
 
-    MIN_DIM = (25, MAX_NAME_LEN+4)
-    screen = curses.newwin(0, 0, 0, 0)
-    height, width = screen.getmaxyx()
 
-    if height < MIN_DIM[0] or width < MIN_DIM[1]:
-        raise "Screen is too small to do stuff!"
+def fuzzy_search(icons, pattern, max_el):
+    """A very basic bur working fuzzy search.
+    The letters of pattern should appear in order, but don't need to be
+    contiguous.
+    i.e. 'cirle' will match 'circle'."""
 
-    curses.start_color()
-    curses.use_default_colors()
-
-    curses.init_pair(1, 48, -1)
-    green_fg = curses.color_pair(1)
-
-    curses.init_pair(2, 75, -1)
-    results_fg = curses.color_pair(2)
-
-    # Title
-    title_h = 5
-    title_w = curses.newwin(title_h, width, 0, 0)
-    title_w.addstr("Press <ESC> to quit")
-    title_w.bkgdset(green_fg)
-    title = "Search  "
-    title_w.addstr(3, ((width - len(title)) // 2), title)
-    title_w.refresh()
-
-    # Search bar
-    bar_h, bar_w = 3, 40
-    bar_x = (width - bar_w) // 2
-    bar_y = 5
-
-    outer = curses.newwin(bar_h, bar_w, bar_y, bar_x)
-    outer.bkgdset(green_fg)
-    outer.refresh()
-
-    # Search result
-    results_y = bar_y + bar_h
-    results_h = height - results_y
-    results = curses.newwin(results_h, width, results_y, 0)
-    results.bkgdset(results_fg)
-
-    search_content = []
-    while True:
-        pattern = "".join(search_content)
-
-        findings = []
-        count = 0
-        for k, v in icons.items():
-            if k.find(pattern) > 0:
-                count += 1
-                if count == (results_h - 2) // 2:
+    findings = []
+    for key in icons.keys():
+        pos = 0
+        found = False
+        for letter in pattern:
+            if letter == " ":
+                continue
+            found = False
+            for i in range(pos, len(key)):
+                if letter == key[i]:
+                    found = True
+                    pos = i + 1
                     break
-                findings.append(k)
+            if not found:
+                break
+        if found:
+            findings.append(key)
 
-        results.clear()
-        display_results_w = MAX_NAME_LEN + 4
-        padding = (width - display_results_w) // 2
-        for i in range(len(findings)):
-            if i < results_h:
-                results.addstr(1 + i * 2, padding, findings[i])
-                results.move(1 + i * 2, padding + MAX_NAME_LEN)
-                # NOTE: for some reason, icons are displayed correctly when
-                # "results" has a bg set ???
-                icon = icons[findings[i]]
-                for c in icon:
-                    results.addch(c)
-                results.addch(" ")
+    # Sort: full matches first.
+    ordered_findings = []
+    for i in range(len(findings)):
+        if findings[i].find(pattern) > 0:
+            ordered_findings.insert(0, findings[i])
+        else:
+            ordered_findings.append(findings[i])
 
-        results.refresh()
-        outer.clear()
-        ui.rounded_box(outer)
-
-        outer.addstr(1, 2, pattern)
-        c = outer.getch()
-        c = chr(c)
-        if c.isalnum() or c in " -_":
-            if len(search_content) < bar_w - 4:
-                search_content.append(c)
-        if c == "\x1b":
-            break
-        elif c in ["\x7f", ""]:
-            if len(search_content):
-                search_content.pop()
-
-
-if __name__ == "__main__":
-    curses.wrapper(main)
-    # count = 0
-    # for _, v in icons.items():
-    #     count += 1
-    #     if count == 10:
-    #         break
-    #     print(v + " ")
+    return ordered_findings[:max_el]
