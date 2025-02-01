cc3d042263e237bb76bca3a2531eeaa4864d2115
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-02-01 18:21:47 -0300

    Take a more OOP-ish aproach: This will simplify code.

--- internal/ncurses/ncurses.go
+++ internal/ncurses/ncurses.go
@@ -40,59 +40,22 @@ func SetLocale(category int, locale string) (string, error) {
 	return lc, nil
 }
 
-func InitScr() {
-	C.initscr()
-}
-
-func AddStr(s string) {
-	cs := C.CString(s)
-	defer C.free(unsafe.Pointer(cs))
-	C.addstr(cs)
-}
-
-func Move(y, x int) {
-	C.move(C.int(y), C.int(x))
-}
-
-func Refresh() {
-	C.refresh()
-}
-
-func Cbreak() int {
-	return int(C.cbreak())
-}
-
-func NoCbreak() int {
-	return int(C.nocbreak())
-}
-
-func Echo() int {
-	return int(C.echo())
-}
-
-func NoEcho() int {
-	return int(C.noecho())
-}
-
-func GetCh() byte {
-	return byte(C.getch())
-}
-
-func EndWin() {
-	C.endwin()
-}
-
-func ScreenSize() (int, int) {
-	return int(C.LINES), int(C.COLS)
-}
-
-// Windows
-
 // Encapsulate the C.WINDOW* here:
 type Window struct {
 	win           *C.WINDOW
 	Height, Width int
 	Y, X          int
+}
+
+func InitScr() Window {
+	// TODO: Check if "stdscr" is NULL.
+	return Window{
+		win:    C.initscr(),
+		Height: int(C.LINES),
+		Width:  int(C.COLS),
+		Y:      0,
+		X:      0,
+	}
 }
 
 func NewWin(height, width, y, x int) Window {
@@ -105,45 +68,50 @@ func NewWin(height, width, y, x int) Window {
 	}
 }
 
-func Box(w Window) {
+// These can alse be used with "stdscr"!
+func (w Window) Box() {
 	C.box(w.win, 0, 0)
 }
 
-func RoundedBox(w Window) {
+func (w Window) RoundedBox() {
 	topleft := "╭"
 	botleft := "╰"
 	topright := "╮"
 	botright := "╯"
 	C.box(w.win, 0, 0)
-	WMove(w, 0, 0)
-	WAddStr(w, topleft)
+	w.Move(0, 0)
+	w.AddStr(topleft)
 
-	WMove(w, 0, w.Width-1)
-	WAddStr(w, topright)
+	w.Move(0, w.Width-1)
+	w.AddStr(topright)
 
-	WMove(w, w.Height-1, 0)
-	WAddStr(w, botleft)
+	w.Move(w.Height-1, 0)
+	w.AddStr(botleft)
 
-	WMove(w, w.Height-1, w.Width-1)
-	WAddStr(w, botright)
+	w.Move(w.Height-1, w.Width-1)
+	w.AddStr(botright)
 }
 
-func WAddStr(w Window, s string) {
+func (w Window) AddStr(s string) {
 	cs := C.CString(s)
 	defer C.free(unsafe.Pointer(cs))
 	C.waddstr(w.win, cs)
 }
 
-func WMove(w Window, y, x int) {
+func (w Window) Move(y, x int) {
 	C.wmove(w.win, C.int(y), C.int(x))
 }
 
-func WRefresh(w Window) {
+func (w Window) Refresh() {
 	C.wrefresh(w.win)
 }
 
-func WGetCh(w Window) byte {
+func (w Window) GetCh() byte {
 	return byte(C.wgetch(w.win))
+}
+
+func (w Window) Clear() {
+	C.wclear(w.win)
 }
 
 // Some colours.
@@ -163,6 +131,51 @@ func InitPair(pair, fg, bg int) {
 	C.init_pair(C.short(pair), C.short(fg), C.short(bg))
 }
 
-func SetPair(w Window, pair int) {
+func(w Window) SetPair( pair int) {
 	C.wattron(w.win, C.COLOR_PAIR(C.int(pair)))
+}
+
+// Just to keep consistency with curses, but these ones are nor really necessary.
+// These are (in C) just macros that call a "w" function with "stdscr" as first argument.
+func AddStr(s string) {
+	cs := C.CString(s)
+	defer C.free(unsafe.Pointer(cs))
+	C.addstr(cs)
+}
+
+func GetCh() byte {
+	return byte(C.getch())
+}
+
+func Move(y, x int) {
+	C.move(C.int(y), C.int(x))
+}
+
+func Refresh() {
+	C.refresh()
+}
+
+// Standalone curses functions.
+func Cbreak() int {
+	return int(C.cbreak())
+}
+
+func NoCbreak() int {
+	return int(C.nocbreak())
+}
+
+func Echo() int {
+	return int(C.echo())
+}
+
+func NoEcho() int {
+	return int(C.noecho())
+}
+
+func EndWin() {
+	C.endwin()
+}
+
+func ScreenSize() (int, int) {
+	return int(C.LINES), int(C.COLS)
 }
