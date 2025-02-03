a1ecb9c42d3404669dc4cd1d144e6ff0024db306
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-02-03 17:47:23 -0300

    Update example.

--- internal/ncurses/tmp/main.c
+++ internal/ncurses/tmp/main.c
@@ -1,10 +1,14 @@
 #include "niceties.h"
 #include <curses.h>
 #include <locale.h>
+#include <stdio.h>
 
 int main() {
 
-	char * loc = setlocale(LC_ALL, "en_US.UTF-8");
+	/*printf("Usin curses: %s\n", NCURSES_VERSION);*/
+	/*return 0;*/
+
+	char* loc = setlocale(LC_ALL, "en_US.UTF-8");
 	if (loc == NULL) {
 		return 42;
 	}
@@ -17,39 +21,50 @@ int main() {
 
 	mvwaddstr(win, 1, 1, loc);
 
-	wgetch(win);
-
 	int border = 0;
 	int c	   = 0;
+	int err	   = 0;
 	while ((c = wgetch(win)) != 'q') {
+		wclear(win);
 		switch (border) {
 		case 0:
-			sharp_box(win);
+			err = sharp_box(win);
 			break;
 		case 1:
-			rounded_box(win);
+			err = rounded_box(win);
 			break;
 		case 2:
-			dotted_box(win);
+			err = dotted_box(win);
 			break;
 		case 3:
-			dotted_rounded_box(win);
+			err = dotted_rounded_box(win);
 			break;
 		case 4:
-			fat_box(win);
+			err = fat_box(win);
 			break;
 		case 5:
-			fat_dotted_box(win);
+			err = fat_dotted_box(win);
 			break;
 		case 6:
-			double_box(win);
+			err = double_box(win);
 			break;
 		}
 		border++;
 		if (border == 7) {
 			border = 0;
 		}
+		mvwprintw(win, 1, 1, "Error: %d", err);
 	}
+
+	RectSize	 size = { .height = 10, .width = 30 };
+	RectPos		 pos  = { .y = 6, .x = 10 };
+	Padding		 pad  = {.top = 1, .right = 2, .bottom = 1, .left = 2};
+	BorderedRect br	  = new_bordered_rect(size, pos, pad, border_dotted);
+	b_rect_border(&br);
+	b_rect_title(&br, -1, 4, " ima box ");
+	b_rect_write(&br, "This is some text. Here you go.");
+	b_rect_refresh(&br);
+	wgetch(br.inner.win);
 
 	echo();
 	nocbreak();
